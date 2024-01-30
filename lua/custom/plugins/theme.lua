1745edf3b1d5dff613415f456764f06d3f08800e
ilMike42 <ilmike_others@icloud.com>
2024-01-30 14:41:20 +0100

    modified theme (trying kanagawa ⛩️)

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,4 +1,25 @@
-return {
+return { 
+  'rebelot/kanagawa.nvim',
+  config = function ()
+    vim.cmd.colorscheme 'kanagawa'
+    require('kanagawa').setup()
+  end
+}
+
+-- return {
+--   { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+--   {
+--     "baliestri/aura-theme",
+--     lazy = false,
+--     priority = 1000,
+--     config = function(plugin)
+--       vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+--       vim.cmd([[colorscheme aura-dark]])
+--     end
+--   }
+-- }
+
+--[[ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
@@ -6,5 +27,5 @@ return {
     config = function()
       vim.cmd.colorscheme 'tokyonight-night'
     end
-}
+} ]]
 
