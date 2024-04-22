13a9f2abe9dd38f5dac8ae34b2c96bb0c0726aa6
ilMike42 <ilmike_others@icloud.com>
2024-04-22 18:16:40 +0200

    cleaned code

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -16,33 +16,3 @@ return {
   end
 }
 
--- return {
---   'rebelot/kanagawa.nvim',
---   config = function ()
---     vim.cmd.colorscheme 'kanagawa'
---     require('kanagawa').setup()
---   end
--- }
-
--- return {
---   { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
---   {
---     "baliestri/aura-theme",
---     lazy = false,
---     priority = 1000,
---     config = function(plugin)
---       vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
---       vim.cmd([[colorscheme aura-dark]])
---     end
---   }
--- }
-
---[[ return {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      vim.cmd.colorscheme 'tokyonight-night'
-    end
-} ]]
